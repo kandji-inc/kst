c65558a8b9f8086b007a7e6269ed3dc1d9b1444e
Steven Boissezon <stevenb@canva.com>
2025-06-02 15:19:30 +1000

    add new imports

--- src/kst/api/__init__.py
+++ src/kst/api/__init__.py
@@ -1,12 +1,16 @@
+from .apps import CustomAppsResource
 from .client import ApiClient, ApiConfig
 from .payload import (
     ApiPayloadType,
+    CustomAppPayload,
+    CustomAppUploadPayload,
     CustomProfilePayload,
     CustomScriptPayload,
     PayloadList,
     SelfServiceCategoryPayload,
 )
 from .profiles import CustomProfilesResource
+from .s3_client import S3ApiClient
 from .scripts import CustomScriptsResource, ExecutionFrequency
 from .self_service import SelfServiceCategoriesResource
 
@@ -14,12 +18,16 @@ __all__ = [
     "ApiClient",
     "ApiConfig",
     "ApiPayloadType",
+    "CustomAppPayload",
+    "CustomAppUploadPayload",
+    "CustomAppsResource",
     "CustomProfilePayload",
     "CustomProfilesResource",
     "CustomScriptPayload",
     "CustomScriptsResource",
     "ExecutionFrequency",
     "PayloadList",
+    "S3ApiClient",
     "SelfServiceCategoriesResource",
     "SelfServiceCategoryPayload",
 ]
